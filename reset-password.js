1ce5224d9cd02b494c91cfcdc1e54fffbf85583f
youngquad <140623986+youngquad@users.noreply.github.com>
2025-04-08 17:04:40 +0100

    Create reset-password.js

---
+++ reset-password.js
@@ -0,0 +1,15 @@
+{
+    "name": "educare-frontend",
+    "version": "1.0.0",
+    "scripts": {
+      "dev": "next dev",
+      "build": "next build",
+      "start": "next start"
+    },
+    "dependencies": {
+      "next": "^13.4.7",
+      "react": "^18.2.0",
+      "react-dom": "^18.2.0",
+      "axios": "^1.3.4"
+    }
+  }
